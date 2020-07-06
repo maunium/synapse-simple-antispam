81db96ab4bcc79a4a9e07f77c265bd86f173e8ba
Tulir Asokan <tulir@maunium.net>
2020-07-06 11:59:43 +0300

    Add option to block specific users

--- synapse_simple_antispam/antispam.py
+++ synapse_simple_antispam/antispam.py
@@ -1,6 +1,7 @@
 class AntiSpamInvites(object):
     def __init__(self, config):
         self._block_invites_from = config.get("blocked_homeservers", [])
+        self._block_invites_from_users = config.get("blocked_users", [])
 
     def check_event_for_spam(self, event):
         return False # not spam
@@ -8,6 +9,9 @@ class AntiSpamInvites(object):
     def user_may_invite(self, inviter_user_id, invitee_user_id, room_id):
         for bad_hs in self._block_invites_from:
             if inviter_user_id.endswith(":" + bad_hs):
+                return False # not allowed
+        for bad_user in self._block_invites_from_users:
+            if inviter_user_id == bad_user:
                 return False # not allowed
         return True # allowed
 
